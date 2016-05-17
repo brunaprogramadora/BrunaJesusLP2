ca85d2c277d5f2a2d5f1d7c7dad4d67338af0d4f
Bruna de Jesus <brunafigueiredoj@hotmail.com>
2016-05-17 15:52:42 -0300

    Adicionando novo trabalho

---
+++ JogoDaForcaa/JogoDaForcaa/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDaForcaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int aux = 0;
+            int x = 0;
+            Console.WriteLine("Escreva palavra misteriosa");
+            string palavra = Console.ReadLine();
+
+            char[] ast = new char[palavra.Length];
+
+            while (aux < 7)
+            {
+                Console.WriteLine("Digite a letra");
+
+                char letra = char.Parse(Console.ReadLine());
+
+                for (int i = 0; i < palavra.Length; i++)
+                {
+                    if (letra == palavra[i])
+                    {
+                        ast[i] = palavra[i];
+                    }
+                    else
+                    {
+                        ast[i] = '*';
+                        aux++;
+                    }
+                    
+                }
+
+                Console.WriteLine(ast);
+                
+            }
+        }
+    }
+}
