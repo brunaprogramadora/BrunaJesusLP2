db0a564d4bc3d6fd909257be13330bea1359231e
brunaprogramadora <brunafigueiredoj@hotmail.com>
2016-06-07 15:51:03 -0300

    Salvando o programa feito na aula do dia 07/06

---
+++ AulaLp2_07-06-2016/AulaLp2_07-06-2016/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaLp2_07_06_2016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo a = new Triangulo(6, 10, 15);
+            
+        }
+    }
+}
---
+++ AulaLp2_07-06-2016/AulaLp2_07-06-2016/Triangulo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaLp2_07_06_2016
+{
+    public class Triangulo
+    {
+        
+        public double Lado1 { get; set; }
+        public double Lado2 { get; set; }
+        public double Lado3 { get; set; }
+        public int Id { get; set; }
+        public int Posx { get; set; }
+        public int Posy { get; set; }
+
+        private int _cor;
+        public int Cor {
+            get { return _cor; }
+            set
+            {
+                if (Cor >= 0 && Cor <= 16777215)
+                    _cor = value;    
+            }
+        }
+
+        public Triangulo(double a, double b, double c)
+        {
+            if (Lado1 < (Lado2 + Lado3) && Lado2 < (Lado1 + Lado3) && Lado3 < (Lado2 + Lado1) && Lado1 > 0 && Lado2 > 0 && Lado3 > 0)
+            {
+                this.Lado1 = a;
+                this.Lado2 = b;
+                this.Lado3 = c;
+            }
+
+        }
+    }
+}
