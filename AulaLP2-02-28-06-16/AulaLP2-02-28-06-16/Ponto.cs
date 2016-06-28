22b5c03f94b5d275ab84f66a93ee038aecc26a04
PaulinhaSigoDeVolta <paulavdutr@gmail.com>
2016-06-28 15:59:39 -0300

    Salvando os programas da aula do 28/06

---
+++ AulaLP2-02-28-06-16/AulaLP2-02-28-06-16/Ponto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaLP2_02_28_06_16
+{
+    class Ponto
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Ponto (int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+
+    }
+}
