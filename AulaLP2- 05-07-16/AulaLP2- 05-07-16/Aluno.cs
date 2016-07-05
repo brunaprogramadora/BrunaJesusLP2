fc3b6e8e82770aa5acf421c3e9615ffa3b8db1f7
PaulinhaSigoDeVolta <paulavdutr@gmail.com>
2016-07-05 16:00:45 -0300

    Adicionando o programa de turmas e alunos.

---
+++ AulaLP2- 05-07-16/AulaLP2- 05-07-16/Aluno.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaLP2__05_07_16
+{
+    class Aluno
+    {
+    }
+}
---
+++ AulaLP2- 05-07-16/AulaLP2- 05-07-16/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaLP2__05_07_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Turma t = new Turma();
+            t.AnoLetivo = "2016";
+            t.Serie = "2° ano";
+            t.Numero = "IN207";
+            t.NumSala = "8";
+            t.Tipo = "Integrado";
+            t.Turno = "Manhã";
+
+            for (int i = 0; i < 3; i++)
+			{
+			    Aluno a = new Aluno();
+                a.Matricula = Console.ReadLine();
+                a.Nome = Console.ReadLine();
+                a.Numero = int.Parse(Console.ReadLine());
+                t.AddAluno(a);
+			}
+            foreach (Aluno a in t.Alunos)
+            {
+                Console.WriteLine("{0} - {1}, {2}", a.Matricula, a.Nome, a.Numero);
+            }
+        }
+    }
+}
